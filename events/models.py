52ab8ea9cd47a0cd44d5fa59111fe0a2c79a5c15
Eddrich Janzzen Ang <eddrichang@gmail.com>
2022-03-31 21:14:43 +0800

    Add initial migration and models for events

--- events/models.py
+++ events/models.py
@@ -1,6 +1,15 @@
+import uuid
 from django.db import models
 
-class Event(models.Model):
+class TimeStampMixin(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+class Event(TimeStampMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     name = models.CharField(max_length=200)
     description = models.CharField(max_length=250)
     date = models.DateField()
