f6951c5a7de81bb8df555599fa10ad7ee5ea7cc7
Eddrich Janzzen Ang <eddrichang@gmail.com>
2022-04-01 20:47:17 +0800

    Override user model to accept only username

--- events/models.py
+++ events/models.py
@@ -1,5 +1,7 @@
 import uuid
 from django.db import models
+from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.utils.translation import gettext_lazy as _
 
 class TimeStampMixin(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
@@ -14,3 +16,57 @@ class Event(TimeStampMixin):
     description = models.CharField(max_length=250)
     date = models.DateField()
     time = models.TimeField()
+
+class UserManager(BaseUserManager):
+
+    use_in_migrations = True
+
+
+    def _create_user(self, email, password, **extra_fields):
+
+        if not email:
+            raise ValueError('The given email must be set')
+        
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+
+        extra_fields.setdefault('is_staff', False)
+        extra_fields.setdefault('is_superuser', False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+
+        extra_fields.setdefault('is_staff', True)
+        extra_fields.setdefault('is_superuser', True)
+
+        if extra_fields.get('is_staff') is not True:
+            raise ValueError('Superuser must have is_staff=True.')
+        
+        if extra_fields.get('is_superuser') is not True:
+            raise ValueError('SuperUser must have is_superuser=True.')
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser, TimeStampMixin):
+    username = None
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    email = models.EmailField(_('email address'), unique=True)
+    display_name = models.TextField(max_length=50)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    avatar = models.TextField(blank=True)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
