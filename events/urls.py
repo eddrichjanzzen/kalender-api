42a668c3d52753360828008c5101ccac8e96cb02
Eddrich Janzzen Ang <eddrichang@gmail.com>
2022-04-01 20:48:26 +0800

    Change path to pk instead of user_id

--- events/urls.py
+++ events/urls.py
@@ -3,5 +3,5 @@ from events.views import user_view
 
 urlpatterns = [
     path('users/', user_view.UserList.as_view()),
-    path('users/<uuid:user_id>', user_view.UserDetail.as_view()),
+    path('users/<uuid:pk>', user_view.UserDetail.as_view()),
 ]
